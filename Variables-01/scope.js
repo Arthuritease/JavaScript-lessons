a9021056cb08f69114300ade83a6a2fc3b4ae4a7
Arthur Koh <arthuritease@Arthuritease-MBP.local>
2022-01-26 14:29:26 +0800

    This puppy be working

---
+++ Variables-01/scope.js
@@ -0,0 +1,7 @@
+// in JavaScript, {...} mforms a scope
+const prompt = require("prompt-sync")();
+let age = parseInt(prompt("Please enter your age: "));
+if (age < 67) {
+  console.log("you cannot retire yet");
+  console.log("Government still want your cpf");
+}
