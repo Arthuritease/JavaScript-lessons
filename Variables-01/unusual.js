2e66d7f7ce024b063a45ee2824785644b94ec447
Arthur Koh <zhaomingkoh.arthur@gmail.com>
2022-01-26 03:31:13 +0000

    things not working..

---
+++ Variables-01/unusual.js
@@ -0,0 +1,5 @@
+let something;
+console.log("something=", something);
+
+let s = something +10
+console.log("s =", s);
---
+++ Variables-01/variables.js
@@ -0,0 +1,21 @@
+
+//strings ---> text
+//"she sells seashell at the seashore"
+
+//integers---> numbers
+//88
+
+//boolean values
+//true or false
+
+//create a js variable
+let ageRightNow = 30; //use camel case for JS
+//imagine console to be a container that can have
+//many  functions, and log is one of them
+//console.log ==> console's log function
+
+//can only 'let' variables once *normally
+//but reassign new variable
+console.log("age =", ageRightNow);
+
+
