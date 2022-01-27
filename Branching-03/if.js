f0d071da3ba34cc60dd97155eac94427d88dc8c5
Arthur Koh <arthuritease@Arthuritease-MBP.local>
2022-01-27 10:41:09 +0800

    Boolean and comparison

---
+++ Branching-03/if.js
@@ -0,0 +1,28 @@
+let password = "momo123";
+if (password === "momo123") {
+  console.log("Login successful!");
+} else {
+  console.log("Sala bruh");
+}
+
+let score = 81;
+
+if (score >= 90) {
+  console.log("aiseh!");
+} else if (score >= 80) {
+  console.log("buay pai");
+} else if (score >= 60) {
+  console.log("pass la pass");
+} else if (score <= 59) {
+  console.log("die bro");
+}
+
+//alternative below
+
+// replicate the above code without using any else-if
+if (score >= 90) {
+  console.log("AD");
+}
+if (score >= 80 && score < 90) {
+  console.log("A");
+}
