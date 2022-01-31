a1c425ef755ace9c9ffbe8ecca90eaa08c9ee03e
Arthuritease <zhaomingkoh.arthur@gmail.com>
2022-01-31 12:34:05 +0800

    comepleted to question 7 of handson

---
+++ Objects-07/nested.js
@@ -0,0 +1,10 @@
+let employee = {
+  firstName: "Jane",
+  lastName: "Doe",
+  address: {
+    street: "Yishun Ring Road",
+    building: "Blk 636",
+    unit: "#13-221",
+  },
+};
+console.log(employee.address.street);
