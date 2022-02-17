d1c1cf9526f9f02edfbe109fc9a4f87e951b86db
Arthuritease <zhaomingkoh.arthur@gmail.com>
2022-02-17 17:36:55 +0800

    i'm back

---
+++ JStest/test.js
@@ -0,0 +1,27 @@
+let sales = [
+  {
+    transactionNo: 1321,
+    salesperson: "Grace",
+    dealAmount: 3700,
+  },
+  {
+    transactionNo: 1571,
+    salesperson: "John",
+    dealAmount: 1200,
+  },
+  {
+    transactionNo: 1321,
+    salesperson: "Melvin",
+    dealAmount: 7500,
+  },
+  {
+    transactionNo: 1200,
+    salesperson: "Melvin",
+    dealAmount: 7500,
+  },
+  {
+    transactionNo: 1210,
+    salesperson: "John",
+    dealAmount: 1200,
+  },
+];
